a01a07408c79e3c6a9effcc7a1d65d57f0ddd96d
SgtGarro <marcelogarro137@gmail.com>
2022-11-28 12:28:01 -0500

    Building my HashTable & implementing methods

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Lists, trees, graphs and more to create more efficient code with JavaScript.
 
+### Array
 1. Building my array & implementing methods
 2. Method "unshift"
 3. Method "shift"
+
+### HashTable
+1. Building my HashTable & implementing methods
--- main.js
+++ main.js
@@ -1,70 +1,24 @@
-class MyArray {
-  constructor() {
-    this.length = 0;
-    this.data = {};
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
   }
-
-  get(index) {
-    return this.data[index];
+  hashMethod(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++)
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    return hash;
   }
-  push(item) {
-    this.data[this.length] = item;
-    this.length++;
+  set(key, value) {
+    const address = this.hashMethod(key);
+    if (!this.data[address]) this.data[address] = [];
+    this.data[address].push([key, value]);
     return this.data;
   }
-  pop() {
-    if (this.length === 0) return;
-    const lastItem = this.data[this.length - 1];
-    delete this.data[this.length - 1];
-    this.length--;
-    return lastItem;
-  }
-  delete(index) {
-    const item = this.data[index];
-    this.shiftIndex(index);
-    return item;
-  }
-  shiftIndex(index) {
-    for (let i = index; i < this.length - 1; i++) {
-      this.data[i] = this.data[i + 1];
-    }
-    delete this.data[this.length - 1];
-    this.length--;
-  }
-
-  unshift(item) {
-    if (!item) return this.length;
-    for (let i = this.length; i > 0; i--) this.data[i] = this.data[i - 1];
-    this.data[0] = item;
-    this.length++;
-    return this.length;
-  }
-  shift() {
-    if (this.length === 0) return undefined;
-    const item = this.data[0];
-    for (let i = 0; i < this.length - 1; i++) {
-      this.data[i] = this.data[i + 1];
-    }
-    delete this.data[this.length - 1];
-    this.length--;
-    return item;
-  }
 }
-const myArray = new MyArray();
-// myArray.push("Jose");
-// myArray.push("Ana");
-// console.log(myArray.get(0));
-// const lastItem = myArray.pop();
-// console.log("POP", lastItem);
-// console.log(myArray.data);
-// myArray.push("Ana");
-// myArray.push("Oscar");
-// console.log(myArray.data);
-// const deletedItem = myArray.delete(1);
-// console.log("Deleted:", myArray.data);
-// console.log("Item deleted:", deletedItem);
-myArray.push("Jose");
-myArray.push("Ana");
-console.log("Normal array", myArray.data);
-myArray.unshift("Oscar");
-console.log("Unshifted array", myArray.data);
+
+const myHashTable = new HashTable(50);
+
+myHashTable.set("Diego", 1990);
+myHashTable.set("Mariana", 1998);
+myHashTable.set("Alejandra", 2000);
+console.log(myHashTable.data);
